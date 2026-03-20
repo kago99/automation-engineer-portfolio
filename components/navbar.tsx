4f65cee5b4c4f375ca9ac58601da2739aadd3598
kago99 <vikinggor48@gmail.com>
2026-03-20 13:21:14 +0300

    navbar heading change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -40,7 +40,7 @@ export function Navbar() {
           <div className="size-10 rounded-lg bg-primary/20 flex items-center justify-center">
             <Zap className="size-5 text-primary" />
           </div>
-          <span className="font-semibold text-lg hidden sm:block">AutomateFlow</span>
+          <span className="font-semibold text-lg hidden sm:block">Lewis Kago</span>
         </a>
 
         {/* Desktop Navigation */}
